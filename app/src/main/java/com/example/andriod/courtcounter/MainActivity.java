d2061f5f1180fd77f641c71524fd61764e02f160
LazarusMoleele <kololomolele@gmail.com>
2017-07-04 09:57:15 +0200

    double check initial commit changes

--- app/src/main/java/com/example/andriod/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/andriod/courtcounter/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,20 +17,56 @@ public class MainActivity extends AppCompatActivity {
 
         displayForTeamA(0);
     }
-
+    /*
+        *iIncrease the score for Team A by 3 points.
+         */
     public void addThreeForTeamA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
-
+    /*
+        *iIncrease the score for Team A by 2 points.
+         */
     public void addTwoForTeamA(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
-
+    /*
+        *iIncrease the score for Team A by 1 points.
+         */
     public void addOneForTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+    }
+    /*
+        *iIncrease the score for Team B by 3 points.
+         */
+    public void addThreeForTeamB (View view){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+    /*
+        *iIncrease the score for Team B by 2 points.
+         */
+    public void addTwoForTeamB (View view){
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+    /*
+    *iIncrease the score for Team B by 1 points.
+     */
+    public void addOneForTeamB (View view){
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+    /*
+    *Resets the score for both teams back to 0.
+     */
+    public void resetScore(View view){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
